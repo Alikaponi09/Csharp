c29046dbdcc71bf35ab3699e57de680715a4fc16
A|R|R <64982730+Alikaponi09@users.noreply.github.com>
2022-06-29 17:27:22 +0300

    Add files via upload

---
+++ Матрица смежности и Флойд/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(204, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать связи";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(645, 231);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Граф ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(305, 175);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(297, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Матрица инцедентности (с весом):";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(355, 301);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(305, 175);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(462, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Поставьте вершины !!!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(351, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(279, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Матрица минимальных растояний:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(342, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 31);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(669, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(389, 508);
+            this.Name = "Form1";
+            this.Text = "Матрица смежности, Давыдова и Адаменко";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Матрица смежности и Флойд/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    //Работу немного переделали прошлогоднюю, тут сразу Флойд.
+    public partial class Form1 : Form 
+    {
+        public Point[] point = new Point[5];
+        public int Kvo = 0, x, y;
+        Graphics graph;
+        Pen n = new Pen(Color.Red, 2);
+        Font font = new Font("Arial", 12);
+        public int[,] matrix, matrix1;
+        public int con = 9999;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Создается матрица смежности, 
+        {
+
+            Random rand = new Random();
+            for (int i = 0; i < Kvo; i++) 
+            {
+                dataGridView1.Columns.Add((i+1).ToString(),(i+1).ToString());
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+            for (int i = 0; i < Kvo; i++) // Заполняется датагрид и рисуются ребра
+            {
+                for (int j = i; j < Kvo; j++)
+                {
+                    if (i != j)
+                    {
+                        int h = rand.Next(0, 5);
+                        if (h > 1) { h = rand.Next(1,10); }
+                        else { h = 0; }
+                        dataGridView1.Rows[i].Cells[j].Value = h;
+                        dataGridView1.Rows[j].Cells[i].Value = h;
+                        if (h != 0) 
+                        {
+                            graph.DrawLine(n, point[i].X + 2, point[i].Y + 2, point[j].X + 2, point[j].Y + 2);
+                        }
+                    }
+                    else { dataGridView1.Rows[i].Cells[j].Value = 0; }
+                }
+            }
+            Vvod();
+            Floyd();
+            Vivod();
+        }
+
+        public void Floyd() // Флойд, который не нужен сейчас, но нужен.
+        {
+            matrix1 = new int[Kvo, Kvo];
+            for (int k = 0; k < Kvo; k++)
+            {
+                for (int i = 0; i < Kvo; i++)
+                {
+                    for (int j = 0; j < Kvo; j++)
+                    {
+                        matrix[i,j] = Min(matrix[i,j], matrix[i,k] + matrix[k,j]);
+                    }
+                }
+            }
+        }
+        public void Vvod() // из датагрид кидаем в обычную матрицу
+        {
+            matrix = new int[Kvo, Kvo];
+            for (int i = 0; i < Kvo; i++)
+            {
+                for (int j = 0; j < Kvo; j++)
+                {
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) == 0) matrix[i, j] = con;
+                    else matrix[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                }
+            }
+        }
+
+        public void Vivod() // из  матрицы вывод в датагрид
+        {
+            for (int i = 0; i < Kvo; i++)
+            {
+                dataGridView2.Columns.Add((i + 1).ToString(), (i + 1).ToString());
+                dataGridView2.Rows.Add();
+                dataGridView2.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+            
+            for (int i = 0; i < Kvo; i++)
+            {
+                for (int j = 0; j < Kvo; j++)
+                {
+                    if (matrix[i, j] == con || i == j) matrix[i,j] = 0;
+                    dataGridView2.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // очищаем(Вы просили, мы сделали)
+        {
+            graph.Clear(Color.White);
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            point = new Point[5];
+            Kvo = 0;
+        }
+
+        public int Min(int x, int y) //Поиск минимума (это функция нужна для Флойдв, сделано тупо, но нам было лень:) )
+        {
+            if (x > y) return y;
+            else return x;
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) //координаты внутри картинки ищет
+        {
+            x = e.X; y = e.Y;
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e) //рисует точки
+        {
+            if (Kvo < 5)
+            {
+                point[Kvo] = new Point(x, y);
+                graph = pictureBox1.CreateGraphics();
+                n = new Pen(Color.Blue, 4);
+                graph.DrawEllipse(n, point[Kvo].X, point[Kvo].Y, 4, 4);
+                graph.DrawString((Kvo + 1).ToString(), font, Brushes.Black, point[Kvo].X - 15, point[Kvo].Y - 15);
+                Kvo++;
+            }
+            else MessageBox.Show("Больше нельзя");
+        }
+    }
+}
